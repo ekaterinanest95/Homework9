3f17af6084e0b43aab8ae02adade1bc50fec434f
Ekaterina <katya.kinal@gmail.com>
2022-09-02 07:30:27 +0300

    Ex66

---
+++ Ex66/Program.cs
@@ -0,0 +1,10 @@
+﻿int M = Convert.ToInt32(Console.ReadLine());
+int N = int.Parse(Console.ReadLine());
+
+int SumOfNumbers(int a, int b)
+{
+    if(a == b) return b;
+    return (a + SumOfNumbers(a+1, b));
+}
+
+Console.WriteLine(SumOfNumbers(M, N));
