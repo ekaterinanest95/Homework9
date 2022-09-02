f92beac8a7ce9697e9e9d11035afc79ea8a8bbcd
Ekaterina <katya.kinal@gmail.com>
2022-09-02 07:47:32 +0300

    Ex68

---
+++ Ex68/Program.cs
@@ -0,0 +1,11 @@
+﻿int M = Convert.ToInt32(Console.ReadLine());
+int N = int.Parse(Console.ReadLine());
+
+int AkkermanFunction(int a, int b)
+{
+    if(a == 0) return b+1;
+    if(b == 0) return AkkermanFunction(a-1, 1);
+    return AkkermanFunction((a-1), AkkermanFunction(a, b-1));
+}
+
+Console.WriteLine(AkkermanFunction(M, N));
